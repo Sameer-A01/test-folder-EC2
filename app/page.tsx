a5220fb36cf7f12120c25f8d85cd006294b780b8
Sameer-A01 <tubesameer786@gmail.com>
2025-11-11 14:31:56 +0530

    deploy ready

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <div className='font-bold text-center text-5xl mt-10'>
       this is my app up and running!
+      this is after the deployment we did on AWS!
     </div>
   )
 }
