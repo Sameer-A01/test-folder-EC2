b9f9cb229e376b6f8128e23a27e6aeec70723fec
Sameer-A01 <tubesameer786@gmail.com>
2025-11-11 14:56:16 +0530

    checking

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ const page = () => {
     <div className='font-bold text-center text-5xl mt-10'>
       this is my app up and running!
       this is after the deployment we did on AWS!
+      This is the final change lets check this time it it works!
+      and yes it has worked!
     </div>
   )
 }
